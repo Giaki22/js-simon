743ca148115c5e35f5b3d871414adad2f1f9d98d
Giaki22 <giaki22@hotmail.com>
2022-05-13 11:05:05 +0200

    + Working with Alerts

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
 let difficulty = difficultyChoice();
 let memo = [];
-let t = 5;
+let t = 30;
 memo = getNumbers(memo, difficulty, t);
-console.log(memo);
+let numberOfCards = difficultyChoice2(difficulty);
+populateDom(numberOfCards, memo);
+const timer = document.getElementById('timer');
 const countdown = setInterval (function(){
-    console.log(t);
+    timer.innerHTML = `TIMER: ${t}s`;
     t--;
     if (t == 0){
+        timer.innerHTML = "";
         clearInterval(countdown);
     }
 }, 1000);
 setTimeout(function() {
-    check(memo);
-}, 5000);
+    hideNumbers();
+}, 29000);
+setTimeout(function() {
+    check(memo, numberOfCards);
+}, 30000);
 
 // FUNCTIONS //
+function populateDom(number, memo){
+    for (let i=0; i<number; i++){
+        let cont = document.getElementById('container');
+        let card = document.createElement('div');
+        card.innerHTML = memo[i];
+        card.classList.add('card');
+        cont.appendChild(card);
+    }
+}
 function getNumbers(array, max ,t){
     while (array.length < t) {
         const randomNumber = Math.floor(Math.random() * max) + 1;
@@ -37,17 +52,30 @@ function difficultyChoice(){
         return 100;
     }
 }
-function check(memo){
+function difficultyChoice2(level){
+    if (level == 50){
+        return 5;
+    } else if (level == 80){
+        return 7;
+    } else if (level == 100){
+        return 10;
+    }
+}
+function check(memo, difficulty){
     let arrayToCheck = [];
-    for (let i=0; i<=5; i++){
+    for (let i=0; i<difficulty; i++){
         do{
             arrayToCheck[i] = Number(prompt("Inserisci un numero"));
         } while (isNaN(arrayToCheck[i]));
-        if (memo.includes(arrayToCheck[i])){
-            continue;
-        } else {
-            alert("HAI PERSO!");
-            break;
-        }
     };
+    let guessedNumbers = [];
+    for (let i=0; i<5; i++){
+        if (memo.includes(arrayToCheck[i]) && !(guessedNumbers.includes(arrayToCheck[i]))){
+            guessedNumbers.push(arrayToCheck[i]);
+        }
+    }
+    alert(`HAI INDOVINATO I SEGUENTI NUMERI: ${guessedNumbers} PER UN PUNTEGGIO DI ${guessedNumbers.length}`);
+};
+function hideNumbers(){
+    document.querySelectorAll('.card').forEach((card) => card.innerHTML = "#?");
 };
